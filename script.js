44a772e0e7f6a9491ab9a26448494f4a56d80ecc
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-06-15 01:23:04 +0200

    emit functioning version

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ function Book(bookId,title,author,pages,readstatus) {
     this.author= author;
     this.pages = pages;
     this.readstatus = readstatus;
-    this.info = function(){
-        if (readstatus) {
-            console.log(this.title+" by "+this.author+", "+this.pages+" pages, already read.")
-        }
-        else {
-            console.log(this.title+" by "+this.author+", "+this.pages+" pages, not read yet.")
-        }
+}
+
+Book.prototype.info = function() {
+    if (this.readstatus) {
+        console.log(this.title+" by "+this.author+", "+this.pages+" pages, already read.")
+    }
+    else {
+        console.log(this.title+" by "+this.author+", "+this.pages+" pages, not read yet.")
     }
 }
 
@@ -25,42 +26,64 @@ function addBookToLibrary(title ,author,pages,readstatus) {
     myLibrary.push(newBook);
 }
 
+const buttonAddBook = document.querySelector(".addBookButton");
+
+buttonAddBook.addEventListener("click", (e) => {
+    e.preventDefault()
+    title = document.querySelector("#title");
+    author = document.querySelector("#author");
+    pages = document.querySelector("#pages");
+
+    readstatus = document.querySelector("input[name='alreadyRead']:checked").value === "true";
+
+    addBookToLibrary(title.value ,author.value,pages.value,readstatus);
+    updateDisplay();
+})
+
+
+
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
 addBookToLibrary("Brave New World", "Aldous Huxley", 268, false);
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 277, false);
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 
-const cardContainer = document.getElementById("cardContainer");
+function updateDisplay() {
+    const cardContainer = document.getElementById("cardContainer");
+    cardContainer.innerHTML = "";
+    for (const book of myLibrary) {
+        const card = document.createElement("table");
 
-for (const book of myLibrary) {
-     const card = document.createElement("table");
+        for (const key in book) {
+            if (typeof book[key] === "function") continue;
+            if (key === "bookId") continue;
 
-     for (const key in book) {
-        if (typeof book[key] === "function") continue;
-        if (key === "bookId") continue;
+            const row = document.createElement("tr");
+            const header = document.createElement("td")
+            header.textContent = key
+            rowValue = document.createElement("td");
+            rowValue.textContent = book[key];
+            row.appendChild(header);
+            row.appendChild(rowValue);
+            card.appendChild(row);
+            
 
-        const row = document.createElement("tr");
-        const header = document.createElement("td")
-        header.textContent = key
-        rowValue = document.createElement("td");
-        rowValue.textContent = book[key];
-        row.appendChild(header);
-        row.appendChild(rowValue);
-        card.appendChild(row);
-        
+        }
+        cardContainer.appendChild(card);
+        //  const row1= document.createElement("tr");
+        //  const header1 = document.createElement("td");
+        //  header1.textContent = "Title"
+        //  const td1 = document.createElement("td");
+        //  td1.textContent = book.title;
 
-     }
-    cardContainer.appendChild(card);
-    //  const row1= document.createElement("tr");
-    //  const header1 = document.createElement("td");
-    //  header1.textContent = "Title"
-    //  const td1 = document.createElement("td");
-    //  td1.textContent = book.title;
+        //  row1.appendChild(header1);
+        //  row1.appendChild(td1);
+        //  card.appendChild(row1);
+        //  cardContainer.appendChild(card);
 
-    //  row1.appendChild(header1);
-    //  row1.appendChild(td1);
-    //  card.appendChild(row1);
-    //  cardContainer.appendChild(card);
+    }
 
 }
+
+
+updateDisplay();
